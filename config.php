2eefbc68847528e9ce9adcfdd3f6695de97d40b4
Roman Pererushev <roman.pererushev@gmail.com>
2023-03-24 18:15:14 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://sitonik/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://sitonik/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/sitonik/catalog/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/sitonik/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/sitonik/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/sitonik/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/sitonik/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/sitonik/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/sitonik/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/sitonik/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/sitonik/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/sitonik/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/sitonik/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'openserver');
+define('DB_USERNAME', 'sitonik');
+define('DB_PASSWORD', '12345');
+define('DB_DATABASE', 'sitonik');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
